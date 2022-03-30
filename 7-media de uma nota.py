93d0bc11ca136e62e8a83ff8f7aad17f76abe633
Juanhen10 <91297117+Juanhen10@users.noreply.github.com>
2022-03-29 23:10:47 -0300

    Create 7-media de uma nota.py

---
+++ 7-media de uma nota.py
@@ -0,0 +1,4 @@
+#faça um program que leia a duas notas de um aluno e descreva a sua media.
+n1 = float(input('Primeira nota: '))
+n2 = float(input('Segunda nota: '))
+print(f'Sua primeira nota foi {n1} e a segunda foi {n2}, sua media ficara {(n1 + n2)/2}')
