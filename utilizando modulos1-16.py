d38ca39768dc69b9cb76bec8565e3e709531c484
Juanhen10 <91297117+Juanhen10@users.noreply.github.com>
2022-03-29 23:32:46 -0300

    Create utilizando modulos1-16.py

---
+++ utilizando modulos1-16.py
@@ -0,0 +1,7 @@
+#crie um programa que leia um numero real qualquer pelo teclado e mostre na tela sua porção inteira
+"""import math
+num = float(input('coloque um numero real '))
+print(f'O valor digitado foi {num} e a sua porçõa inteira é {math.trunc(num)}')"""
+
+num = float(input('Digite um valor: '))
+print(f'o valor digitado foi {num} e sua porção inteira é {int(num)}')
