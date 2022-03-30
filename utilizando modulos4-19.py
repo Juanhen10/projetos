0addbd4711e6cb410da08a558dc5af4ed2ba3afc
Juanhen10 <91297117+Juanhen10@users.noreply.github.com>
2022-03-29 23:34:29 -0300

    Create utilizando modulos4-19.py

---
+++ utilizando modulos4-19.py
@@ -0,0 +1,10 @@
+#Professor quer sortear um dos seus quatro alunos para apagar o quadro.
+#Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido
+import random
+n1 = str(input('Primeiro Aluno: '))
+n2 = str(input('Segundo Aluno: '))
+n3 = str(input('terceiro Aluno: '))
+n4 = str(input('Quarto aluno:  '))
+lista = [n1,n2,n3,n4]
+escolhido = random.choice(lista)
+print(f'o aluno escolhido foi {escolhido}')
