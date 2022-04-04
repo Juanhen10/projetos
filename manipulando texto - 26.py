f3c87b8eaa9ee7b7f25919650f020850cf4a7f47
Juanhen10 <91297117+Juanhen10@users.noreply.github.com>
2022-04-04 09:55:13 -0300

    Create manipulando texto - 26.py

---
+++ manipulando texto - 26.py
@@ -0,0 +1,8 @@
+#Faça um programa que leia uma frase pelo teclado e mostre:
+#Quantas vezes aparece a letra 'a'
+#em que posicão ela aparece a preira vez.
+#em que posição ela aparece a ultima vez
+frase = str(input('Digite uma frase: ')).upper()
+print(f'A letra A aparece {frase.count("A")} vezes na frase')
+print(f'A primeira letra A apareceu na posição {frase.find("A")+1}')
+print(F'A ultima vez que aparece a letra A é {frase.rfind("A")+1}')
