e8cfaa70436375ab4292a82b946bed6a35e2dce0
Juanhen10 <91297117+Juanhen10@users.noreply.github.com>
2022-04-04 09:54:52 -0300

    Create manipulando texto - 25.py

---
+++ manipulando texto - 25.py
@@ -0,0 +1,4 @@
+#Crie um progrtama que leia o nome de uma pessoa e diga se ela tem 'Silva' no nome
+nome = str(input('qual seu nome? '))
+a = 'Silva' in nome
+print(f'Teu nome é {nome} teu nome tem Silva: {a} ')
