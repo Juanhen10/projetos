3572998f91b76b3a9769fd5850f2ad2477b00d5e
Juanhen10 <91297117+Juanhen10@users.noreply.github.com>
2022-04-06 09:57:15 -0300

    Update Condições(part1)028.py

--- Condições(part1)028.py
+++ Condições(part1)028.py
@@ -1,3 +1,6 @@
+#Escreva um programa que faça o computador "pensar" em um numero inteiro entre 0 e 5
+#e peça oara o usuario tentar descobrir qual foi o númeor escolhido pelo computador. 
+#o programa deverá escrever na tela se o usuario venceu ou não
 import random
 pc = random.randint(0,5)
 a = int(input('escolha um numero de 1 a 5: '))
