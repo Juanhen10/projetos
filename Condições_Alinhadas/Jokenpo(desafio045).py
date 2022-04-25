e3110b0fc821caafb380d5775094be7e311092ad
Juanhen10 <91297117+Juanhen10@users.noreply.github.com>
2022-04-25 08:40:10 -0300

    Update Jokenpo(desafio045).py

--- Condições_Alinhadas/Jokenpo(desafio045).py
+++ Condições_Alinhadas/Jokenpo(desafio045).py
@@ -1,10 +1,17 @@
 #crie um programa que faça o computador jogar Jokenpo com você
 import random
+from time import sleep
 jokenpo = str(input('Escolha \033[31mPEDRA\033[m, \033[32mTesoura\033[m ou \033[33mPapel\033[m: '))
 papel = 1
 pedra = 2
 tesoura = 3
 pc = random.randint(1,3)
+print(f'{"JO"}')
+sleep(1)
+print(f'{"KEN"}')
+sleep(1)
+print(f'{"PO!!"}')
+sleep(1)
 if pc == papel:
     print('Escolhi PAPEL!')
 elif pc == pedra:
@@ -14,7 +21,7 @@ elif pc == tesoura:
 if jokenpo == 'papel' and pc == pedra:
     print('Você ganhou')
 elif pc == papel and jokenpo == 'pedra':
-    print('Eu ganhaei')
+    print('Eu ganhei')
 elif jokenpo == 'tesoura' and pc == papel:
     print('Você ganhou')
 elif pc == tesoura and jokenpo == papel:
