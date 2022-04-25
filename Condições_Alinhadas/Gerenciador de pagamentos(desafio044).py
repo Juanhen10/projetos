fce29a21d8a37a9e84d56b3667eaec6645673872
Juanhen10 <91297117+Juanhen10@users.noreply.github.com>
2022-04-25 08:29:27 -0300

    Update Gerenciador de pagamentos(desafio044).py

--- Condições_Alinhadas/Gerenciador de pagamentos(desafio044).py
+++ Condições_Alinhadas/Gerenciador de pagamentos(desafio044).py
@@ -4,22 +4,20 @@
 #a vista no cartão: 5% de desconto
 #em até 2x no cartão: preço normal
 #3x ou mais no cartão: 20% de juros
-produto = float(input('Quanto custa o produto? '))
+produto = float(input('Quanto custa o produto? R$ '))
 forma = str(input('Qual forma de pagamento? '))
 valor = produto
 dinheiro = valor * 0.90
 cartao = valor * 0.95
 cartao2x = valor / 2
-cartao3x = (valor / 3) + (valor * 1.20)
-'''print(dinheiro)
-print(cartao)
-print(cartao2x)
-print(cartao3x)'''
+cartao3x = valor + (valor * 20 / 100)
 if 'dinheiro' == forma:
-    print(f'o produto em forama de dinheiro, custará R${dinheiro}')
+    print(f'o produto em forma de dinheiro, custará R${dinheiro}')
 elif 'cartao' == forma:
     print(f'o produto em forma de cartão custará R${cartao}')
 elif 'cartao2x' == forma:
     print(f'o produto em 2x no cartão custará R${cartao2x}')
 elif 'cartao3x' == forma:
+    totalpatc = int(input('Quantas parcelas? '))
+    parcela = valor / totalpatc
     print(f'o produto em 3x no cartão custára R${cartao3x:.2f}')
