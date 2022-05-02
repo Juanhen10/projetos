e320ad2b1b5a1a495b3394c5021c4b6500fe7d46
Juanhen10 <91297117+Juanhen10@users.noreply.github.com>
2022-05-02 08:58:32 -0300

    Update Maior e menor da sequência(desafio054).py

--- estrutura de repetição/Maior e menor da sequência(desafio054).py
+++ estrutura de repetição/Maior e menor da sequência(desafio054).py
@@ -1,12 +1,17 @@
+
 #faça um programa que leia  o peso de cinco pessoas. No final mostre qual foi o maior e
 #menor pesos lidos
 totmaior = 0
 totmenor = 0
 for a in range(1, 6):
-    peso = int(input(f'Qual o peso da {a}º pessoa? KG'))
-    if peso > 25:
-        totmaior += 1
-    elif peso < 25:
-        totmenor += 1
-print(f'Temos {totmaior} pessoas com pesos maiores')
-print(f'E temos temos {totmenor} com pesos menores')
+    peso = float(input(f'Qual o peso da {a}º pessoa? KG'))
+    if a == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print(f'O maior peso lido foi \033[34m{maior}KG\033[m ')
+print(f'O menor peso lido foi de \033[32m{menor}KG\033[32m')
