fb0b7b339a522b746ac321ce881c25e8ea9a3b3f
Juanhen10 <91297117+Juanhen10@users.noreply.github.com>
2022-05-03 11:09:49 -0300

    Delete conversor de moeda-10.py

--- conversor de moeda-10.py
+++
@@ -1,8 +0,0 @@
-#conversor de moeda
-real = float(input('qunatos dinheiro você tem na carteira?R$ '))
-dolar = real / 4.98
-euro = real /5.50
-libra = real / 6.57
-print(f'com R${real :.2f} você pode comprar US${dolar :.2f}')
-print(f'com R${real :.2f} você pode comprar EUR${euro :.2f}')
-print(f'com R${real :.2f} você pode comprar E${libra :.2f}')
