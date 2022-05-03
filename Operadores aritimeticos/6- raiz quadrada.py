030e6f8ec254841ba1d629a33e2453363405ce14
Juanhen10 <91297117+Juanhen10@users.noreply.github.com>
2022-05-03 11:08:07 -0300

    Create 6- raiz quadrada.py

---
+++ Operadores aritimeticos/6- raiz quadrada.py
@@ -0,0 +1,6 @@
+#crie um algoritmo que leia um numero e mostre o seu dobro, triplo e raiz quadrada
+n1 = int(input('colque um numero: '))
+m = n1 * 2
+d = n1 * 3
+t = n1 ** (1/2)
+print(f"o dobro de {n1} é {m} o triplo de {n1} é {d} e a raiz quadrada é {t:.3f}")
