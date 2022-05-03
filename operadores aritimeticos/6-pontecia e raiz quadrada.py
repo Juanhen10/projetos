bb96e40fd7bced6138fbf50748f16a81d27bdf14
Juanhen10 <91297117+Juanhen10@users.noreply.github.com>
2022-05-03 10:54:00 -0300

    Delete operadores aritimeticos directory

--- operadores aritimeticos/6-pontecia e raiz quadrada.py
+++
@@ -1,3 +0,0 @@
-#faça um programa que leia o dobro do numero, o triplo e a raiz quadrada(modo simplificado
-n = int(input('Coloque um numero: '))
-print(f'o dobro desse número é {n*2}, o triplo é {n*3} e a raiz quadrada é {n **(1/2)}')
