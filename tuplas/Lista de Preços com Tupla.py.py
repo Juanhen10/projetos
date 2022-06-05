715f1eb68eb0a0715f4571af6e3450331c4b4c7a
Juanhen10 <91297117+Juanhen10@users.noreply.github.com>
2022-06-05 00:35:48 -0300

    Create Lista de Preços com Tupla.py.py

---
+++ tuplas/Lista de Preços com Tupla.py.py
@@ -0,0 +1,17 @@
+#Crie um programa que tenha uma tupla unica com nomes de produtos e seus respectivos preços na sequencia.
+#No final, mostre uma listagem de preços organizando  os dados em forma tabular
+listagem = ('Civic', 15.000,'Golf Gti',20.000,'Astra', 15.900, 'Lancer',25.000,'Camaro',40.200,'Opala',90.999,
+            'tesla',120.320,'Primas',10.350,'Onix',34.90)
+print('\033[32m-----\033[m'*10)
+print('\033[36mLISTAGEM DOS PREÇOS DOS CARROS')
+print('\033[32m-----\033[m'*10)
+print(f'\033[35m{listagem[0]}................\033[31mR${listagem[1]}K\033[m')
+print(f'\033[39m{listagem[2]}.............\033[31mR${listagem[3]}K\033[m')
+print(f'\033[37m{listagem[4]}................\033[31mR${listagem[5]}K\033[m')
+print(f'\033[32m{listagem[6]}...............\033[31mR${listagem[7]}K\033[m')
+print(f'\033[35m{listagem[8]}...............\033[31mR${listagem[9]}K\033[m')
+print(f'\033[34m{listagem[10]}................\033[31mR${listagem[11]}K\033[m')
+print(f'\033[33m{listagem[12]}................\033[31mR${listagem[13]}K\033[m')
+print(f'\033[34m{listagem[14]}...............\033[31mR${listagem[15]}K\033[m')
+print(f'\033[32m{listagem[16]}.................\033[31mR${listagem[17]}K\033[m')
+print(print('\033[32m----'* 10))
